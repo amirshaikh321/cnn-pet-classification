017babc8cea4b6cdb185a88814460e2660042215
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-15 17:51:37 +0530

    Predict file updated

--- predict.py
+++ predict.py
@@ -1,6 +1,12 @@
 import torch
 from model import BetterCNN
-
+import os
 model = BetterCNN()
-model.load_state_dict(torch.load("pet_cnn.pth", map_location="cpu"))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Build the full path to the model file
+model_path = os.path.join(current_dir, "pet_cnn.pth")
+
+# Load using the full path
+model.load_state_dict(torch.load(model_path, map_location="cpu"))
 model.eval()
